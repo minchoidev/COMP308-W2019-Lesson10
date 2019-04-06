3d828b531dc22189ae8e3d655faafc28c37ad05c
Minseok Choi <minchoidev@gmail.com>
2019-04-05 20:35:28 -0400

    route guard comments updated

--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -16,6 +16,7 @@ export class AuthGuard implements CanActivate {
     private router: Router
     ) {}
 
+  // if not authorized, then route to /login
   canActivate(): boolean {
     if(this.authService.loggedIn()) {
       return true;
